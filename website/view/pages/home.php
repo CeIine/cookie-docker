0ddd94088d52c7fe5982be2a691dfad10063db05
Céline <liuceline2001@yahoo.fr>
2022-11-22 22:06:36 +0100

    home

---
+++ website/view/pages/home.php
@@ -0,0 +1,13 @@
+<div class="container-fluid row align-items-center text-center section-accueil">
+    <div class="col-5"> 
+        <h1 class="text-light fs-1">
+            <span class="auto-type text-light"></span>
+        </h1> 
+    </div>
+    <div class="col-7 ps-5">
+        <img src="./view/img/cookie-accueil.jpg" class="img-fluid" alt="cookie" >
+    </div>
+</div>
+<div class="container">
+    <h1 class="text-light fs-1 text-center">Best Seller</h1>
+</div>
